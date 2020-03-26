3fc5ccb03807ae2abca5abb609941a2acd9ce7bf
Dakota Wicker <dwicker1996@gmail.com>
2020-03-26 16:49:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+katz_deli =[]
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+     new_arr = []
+     katz_deli.each_with_index {|person, index| new_arr << "#{index+1}. #{person}"}
+     puts "The line is currently: #{new_arr.join(' ')}"
+end
+end
+
+def take_a_number(katz_deli)
+
+end
+
+def now_serving(katz_deli)
+
+
+end
