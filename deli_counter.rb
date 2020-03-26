42fc0c335232b8805b529f05d2bd3275c2c20a4e
Dakota Wicker <dwicker1996@gmail.com>
2020-03-26 19:08:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,21 +6,21 @@ def line(katz_deli)
      new_arr = []
      katz_deli.each_with_index {|person, index| new_arr << "#{index+1}. #{person}"}
      puts "The line is currently: #{new_arr.join(' ')}"
-end
-end
-
-def take_a_number(katz_deli, new_customer)
-
-
-  puts "Welcome, #{new_customer}. You are #{katz_deli.length} in line."
+  end
 end
 
+# def take_a_number(katz_deli, new_customer)
+# 
+# 
+#   puts "Welcome, #{new_customer}. You are #{katz_deli.length} in line."
+# end
 
-def now_serving(katz_deli)
-if katz_deli == <1
-   puts "There is nobody waiting to be served!"
- else
- puts "Currently serving {first.katz_deli}"
- # shift.katz_deli
-end
-end
+
+# def now_serving(katz_deli)
+# if katz_deli == <1
+#    puts "There is nobody waiting to be served!"
+#  else
+#  puts "Currently serving {first.katz_deli}"
+#  # shift.katz_deli
+# end
+# end
